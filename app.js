7853f1edd3bbe587c06b6f1f23556a5ad600af31
ansharahamin <insharahamin1250@gmail.com>
2026-04-01 16:01:53 +0500

    renderQuiz on screen

--- app.js
+++ app.js
@@ -25,3 +25,28 @@ const quiz = [
         answer: "Sun Microsystems"
     }
 ];
+index = 0
+function renderQuiz() {
+    var quizContainer = document.getElementById("cont")
+    console.log(quizContainer);
+    quizContainer.innerHTML = `<div class="card m-5">
+    <h5 class="card-header">${quiz[index].question}</h5>
+    <div class="card-body">
+     
+       <ul class="list-group list-group-flush">
+      <li class="list-group-item"><label for="opt1"><input type="radio" name="option" id="opt1"> ${quiz[index].options[0]}</label></li>
+      <li class="list-group-item"><label for="opt2"><input type="radio" name="option" id="opt2"> ${quiz[index].options[1]}</label></li>
+      <li class="list-group-item"><label for="opt3"><input type="radio" name="option" id="opt3"> ${quiz[index].options[2]}</label></li>
+      <li class="list-group-item"><label for="opt4"><input type="radio" name="option" id="opt4"> ${quiz[index].options[3]}</label></li>
+      <button class="btn btn-success text-center mt-3 mx-auto w-25" onclick = "renderQuiz()">Next</button>
+     
+    </ul>
+    </div>
+  </div>`
+  index++
+}
+renderQuiz()
+
+function next() {
+   
+}
