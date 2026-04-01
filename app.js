ffce4c63ec2032461a5d528013a37b7750b28c86
ansharahamin <insharahamin1250@gmail.com>
2026-04-01 15:38:20 +0500

    index-file-added

---
+++ app.js
@@ -0,0 +1,27 @@
+const quiz = [
+    {
+        question: "What is the capital of France?",
+        options: ["Berlin", "Madrid", "Paris", "Rome"],
+        answer: "Paris"
+    },
+    {
+        question: "Which language is used for web apps?",
+        options: ["Python", "JavaScript", "C++", "Java"],
+        answer: "JavaScript"
+    },
+    {
+        question: "What does HTML stand for?",
+        options: [
+            "Hyper Text Markup Language",
+            "High Text Machine Language",
+            "Hyper Tool Multi Language",
+            "None of these"
+        ],
+        answer: "Hyper Text Markup Language"
+    },
+    {
+        question: "Which company developed Java?",
+        options: ["Microsoft", "Sun Microsystems", "Google", "Apple"],
+        answer: "Sun Microsystems"
+    }
+];
