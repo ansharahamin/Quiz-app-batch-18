966935968ec588ffe70f87e78ef91c7ed0ddf500
ansharahamin <insharahamin1250@gmail.com>
2026-04-20 17:04:11 +0500

    added login Signup with local storage

--- main.js
+++ main.js
@@ -10,3 +10,42 @@ registerBtn.addEventListener('click', () => {
 loginBtn.addEventListener('click', () => {
     container.classList.remove('active');
 })
+var L_email = document.getElementById("L_email");
+var L_password = document.getElementById("L_password");
+// var S_username = document.getElementById("S_username");
+// var S_email = document.getElementById("S_Email");
+// var S_password = document.getElementById("S_password");
+
+function register() {
+    var S_username = document.getElementById("S_username");
+var S_email = document.getElementById("S_Email");
+var S_password = document.getElementById("S_password");
+    if (S_email.value == "" || S_password.value == "" || S_username.value == "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+   var getData = JSON.parse(localStorage.getItem("user"));
+var storage = []
+var user = {
+    username: S_username.value,
+    email: S_email.value,
+    password: S_password.value
+}
+localStorage.setItem("user", JSON.stringify(user));
+storage.push(user);
+window.location.href = "./dashboard.html";
+}
+function login() {
+    var getData = JSON.parse(localStorage.getItem("user"));
+    if (L_email.value == "" || L_password.value == "") {
+        alert("Please fill in all fields.");
+        return false;
+    }
+    if (L_email.value === getData.email && L_password.value === getData.password) {
+        alert("Login successful!");
+        window.location.href = "./dashboard.html";
+    } else {
+        alert("Invalid email or password.");
+    }
+
+}
